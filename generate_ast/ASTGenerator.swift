feede6d1cc8f7b324f2c57969550b9899f97023b
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2021-08-02 18:04:06 -0400

    Add return statement to ASTGenerator

--- generate_ast/ASTGenerator.swift
+++ generate_ast/ASTGenerator.swift
@@ -33,6 +33,7 @@ class ASTGenerator {
                     "Function   : Token name, [Token] params, [Stmt] body",
                     "If         : Expr condition, Stmt thenBranch, Stmt? elseBranch",
                     "Print      : Expr expression",
+                    "Return     : Token keyword, Expr? value",
                     "Var        : Token name, Expr? initializer",
                     "While      : Expr condition, Stmt body"
                   ])
