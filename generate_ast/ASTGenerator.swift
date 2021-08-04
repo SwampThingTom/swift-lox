125092e29dba999e965a73e4f81b6c16f5d25977
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2021-08-03 20:57:08 -0400

    Add class declaration to ASTGenerator

--- generate_ast/ASTGenerator.swift
+++ generate_ast/ASTGenerator.swift
@@ -29,6 +29,7 @@ class ASTGenerator {
                   baseName: "Stmt",
                   types: [
                     "Block      : [Stmt] statements",
+                    "Class      : Token name, [Stmt.Function] methods",
                     "Expression : Expr expression",
                     "Function   : Token name, [Token] params, [Stmt] body",
                     "If         : Expr condition, Stmt thenBranch, Stmt? elseBranch",
